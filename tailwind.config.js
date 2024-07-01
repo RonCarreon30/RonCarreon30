193d6a683a129bd7c1d61be82804ad170aa3ab34
RonCarreon30 <162035736+RonCarreon30@users.noreply.github.com>
2024-07-01 17:48:17 +0800

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./build/*.html'],
+  // Specify the files Tailwind CSS should scan for utility classes
+  content: ["./build/**/*.{html,js,php}"],
+
+  // Extend the default theme with customizations
   theme: {
     extend: {
+      // Define custom colors
+      colors: {
+        "plv-blue": "#001663",
+        "plv-highlight": "#4c4c70",
+        "persian-blue": "#002ccc",
+      },
+      // Define background images
       backgroundImage: {
-        'hero-pattern': "url('/img/bg-image.jpg')",
-      }
+        "hero-pattern": "url('/img/bg-image.jpg')",
+      },
     },
   },
-  plugins: [],
-}
+
+  // Specify any additional plugins (if needed)
+  plugins: ["prettier-plugin-tailwindcss"],
+};
